e754dbfa999204227af581e4351eb8307943fa4f
i202417 <95436786+Ammar123890@users.noreply.github.com>
2023-11-26 15:48:09 +0500

    cookies middleware

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,40 @@
-//NPM Packages
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const cors = require("cors");
+const cookieParser = require('cookie-parser');
 require("dotenv").config();
 const app = express();
-
-
-//Project files and routes
 const apiRouter = require("./Routes");
 const connect = require("./Config/database");
-
-
 
 // Initialize session middleware
 app.use(
     session({ secret: process.env.SECRET, resave: true, saveUninitialized: true })
-  );
+);
 
-
-//connect to database
+// Connect to database
 connect();
 
-//Utils
+// Utils
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+app.use(cookieParser());
 
-//connecting routes
+// Configure CORS
+const corsOptions = {
+  origin: 'http://192.168.0.101:5173', // Replace with your frontend URL
+  credentials: true, // To allow sending of cookies
+};
+app.use(cors(corsOptions));
+
+// Connecting routes
 app.use("/api", apiRouter);
 
-//Connect Server
+// Connect Server
 const PORT = process.env.PORT || 5000;
- app.listen(PORT, () => {
-  console.log(`Your app is running on PORT ${PORT}`);
+app.listen(PORT, () => {
+ console.log(`Your app is running on PORT ${PORT}`);
 });
-
-
