81fae69b7171959c59f99d2f22422ee2feed938d
Hadia <95401282+HadiaTanveer@users.noreply.github.com>
2024-03-29 12:04:00 +0500

    errors resolved again

--- customer-frontend/vite.config.ts
+++ customer-frontend/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [react(),],
+  plugins: [react()],
   define: { 'process.env': {}, },
   resolve: {
     alias: {
